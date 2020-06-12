7eb53174935e1bb18cc377d430bff1e0b76c9d32
maytinhdibo <iammaytinhdibo@gmail.com>
2020-06-12 15:15:02 +0700

    Multi filter

--- filter.js
+++ filter.js
@@ -13,9 +13,9 @@ function sumString(str) {
   return sum;
 }
 
-exportToCsv = function(typename, content, data) {
+exportToCsv = function (typename, content, data) {
   var CsvString = content;
-  data.forEach(function(ColItem, ColIndex) {
+  data.forEach(function (ColItem, ColIndex) {
     CsvString += ColItem + "\r\n";
   });
 
@@ -51,14 +51,14 @@ function abxyFilter() {
   exportToCsv(
     "Lọc tổng đầu cuối",
     "Các số đã được lọc với quy tắc " +
-      a +
-      " số đầu tổng bằng " +
-      x +
-      " và " +
-      b +
-      " số cuổi tổng bằng " +
-      y +
-      " \r\n",
+    a +
+    " số đầu tổng bằng " +
+    x +
+    " và " +
+    b +
+    " số cuổi tổng bằng " +
+    y +
+    " \r\n",
     result
   );
 }
@@ -84,9 +84,9 @@ function lastFilter() {
 
 //filter with price
 
-exportToCsvObj = function(typename, content, data) {
+exportToCsvObj = function (typename, content, data) {
   var CsvString = content + "SIM, GIÁ\r\n";
-  data.forEach(function(ColItem, ColIndex) {
+  data.forEach(function (ColItem, ColIndex) {
     CsvString += ColItem.sim + " ," + ColItem.price + "\r\n";
   });
 
@@ -122,14 +122,55 @@ function abxyFilterObj() {
   exportToCsvObj(
     "Lọc tổng đầu cuối",
     "Các số đã được lọc với quy tắc " +
-      a +
-      " số đầu tổng bằng " +
-      x +
-      " và " +
-      b +
-      " số cuổi tổng bằng " +
-      y +
-      " \r\n",
+    a +
+    " số đầu tổng bằng " +
+    x +
+    " và " +
+    b +
+    " số cuổi tổng bằng " +
+    y +
+    " \r\n",
     result
   );
 }
+
+
+function abxyFilterObjArray() {
+  if (isNull()) return 0;
+  var a = document.querySelector("#a1").value;
+  var b = document.querySelector("#b1").value;
+  var x = document.querySelector("#xArray").value;
+  var y = document.querySelector("#yArray").value;
+
+  var xArray = x.split(",");
+  var yArray = y.split(",");
+
+  if (xArray.length != yArray.length) {
+    return alert("Vui lòng nhập chuỗi xy cùng số lượng");
+  }
+
+  var CsvString = "";
+
+  for (let index = 0; index < xArray.length; index++) {
+    let result = dataCHObj.filter(item => {
+      return (
+        sumString(item.sim.substr(0, a)) == xArray[index] &&
+        sumString(item.sim.substr(item.sim.length - b, item.sim.length - 1)) == yArray[index]
+      );
+    });
+
+    let content = "Các số đã được lọc với quy tắc " + a + " số đầu tổng bằng " + xArray[index] + " và " + b + " số cuổi tổng bằng " + yArray[index] + " \r\n";
+    CsvString += content + "SIM, GIÁ\r\n";
+    console.log(result);
+    result.forEach(function (ColItem, ColIndex) {
+      CsvString += ColItem.sim + " ," + ColItem.price + "\r\n";
+    });
+  }
+  CsvString = "data:text/csv," + encodeURIComponent(CsvString);
+  var x = document.createElement("A");
+  x.setAttribute("href", CsvString);
+  x.setAttribute("download", "Lọc tổng đầu cuối" + ".csv");
+  document.body.appendChild(x);
+  x.click();
+}
+
